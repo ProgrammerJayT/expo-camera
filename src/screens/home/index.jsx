51e54d8ffc5c6ca94dbedfc0fbc82c61c45d85d7
JP Tiyasi <theanthem8@gmail.com>
2025-03-12 18:42:38 +0000

    Create home screen and styles

---
+++ src/screens/home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { styles } from "./styles";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={[styles.container]}>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
