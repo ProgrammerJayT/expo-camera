6856690f38d9d5aaff4124fa348ac6da88b745df
JP Tiyasi <theanthem8@gmail.com>
2025-03-12 20:20:16 +0000

    Add home screen buttons

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,11 +1,37 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Button, SafeAreaView, Text, View } from "react-native";
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const buttons = [
+    { title: "Scan QR Code", path: "Scan QR Code" },
+    { title: "Scan License", path: "Scan License" },
+    { title: "Scan Permit", path: "Scan Permit" },
+  ];
+
   return (
     <SafeAreaView style={[styles.container]}>
       <Text>Home Screen</Text>
+
+      <View></View>
+
+      <View style={[styles.buttonsContainer]}>
+        {(buttons ?? []).map((btn, index) => (
+          <View key={index} style={[styles.buttonContainer]}>
+            <Button
+              onPress={() => {
+                navigation.navigate(btn.path)
+              }}
+              title={btn.title}
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+        ))}
+      </View>
     </SafeAreaView>
   );
 };
