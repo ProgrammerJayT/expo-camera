447e09b51dda280f6ac7eb3855abb40ab7bf5844
JP Tiyasi <theanthem8@gmail.com>
2025-03-12 18:40:13 +0000

    Create qr code scanner screen and styles

---
+++ src/screens/qr-code/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { styles } from "./styles";
+
+const QrCodeScreen = () => {
+  return (
+    <SafeAreaView style={[styles.container]}>
+      <Text>QR Code Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default QrCodeScreen;
