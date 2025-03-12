d7d957656a3db2d341fd2531330b761d2b35dbd8
JP Tiyasi <theanthem8@gmail.com>
2025-03-12 18:41:30 +0000

    Create permit scanner screen and styles

---
+++ src/screens/permit/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const PermitScreen = () => {
+  return (
+      <SafeAreaView style={[styles.container]}>
+      <Text>Permit Scan Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PermitScreen;
