31ad3fdd5156da83eb50a88377919185b0e88cbe
JP Tiyasi <theanthem8@gmail.com>
2025-03-12 18:59:50 +0000

    Create license scanner screen and styles

---
+++ src/screens/license/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { styles } from "./styles";
+
+const LicenseScreen = () => {
+  return (
+    <SafeAreaView style={[styles.container]}>
+      <Text>LicenseScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LicenseScreen;
